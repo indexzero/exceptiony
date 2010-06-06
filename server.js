dbcb8858cb829ff0bfa7f3e02288aaae5c2017cd
indexzero <charlie.robbins@gmail.com>
2010-06-06 18:00:16 -0400

    Convert tabs to spaces

--- server.js
+++ server.js
@@ -18,57 +18,57 @@ require('proto');
 var pathExpression = /^(\/[^\/]+)(.*)$/;
 
 var server = http.createServer(function (request, response) {
-	var httpParams = querystring.parse(request.body);
-	httpParams.mixin(url.parse(request.url));
-	request.params = httpParams;
+  var httpParams = querystring.parse(request.body);
+  httpParams.mixin(url.parse(request.url));
+  request.params = httpParams;
 
-	if ('/' !== request.url) {
-	  var path = request.url.match(pathExpression);
-	}
-	else {
-	  var path = ['/', '/', ''];
-	}
-	
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+  if ('/' !== request.url) {
+    var path = request.url.match(pathExpression);
+  }
+  else {
+    var path = ['/', '/', ''];
+  }
+
+  response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Foliage Remote Serving Request for: ' + path[1]);
-	
-	switch(path[1]) {
-		case '/log' :
-		  var now = new Date().getTime();
-		  var logFile = __dirname + '/session-logs/session-log-' + now + '.json';
-		  //response.write("\n\nApplication path: " + __dirname);
-		  //response.write('\n\nSaving log data to: ' + logFile + ' ...');
-			var postData = ''; 
 
-		  request.addListener("data", function (chunk) {
-			  postData += chunk;
-	    });
-	
-	    request.addListener("end", function () {
-		    if(postData && postData.length > 0) {
-		      fs.open(logFile, "w+", 0644, function (error, fd) {
-			    	if (error) throw error;
-					
-					  fs.writeSync(fd, postData, 0, 'ascii');
-				    response.write('\n\nLog data saved successfully');
-					  response.write('\n\nLog Data: \n' + postData);
-		      	response.end();
-		    	});
-				}
-			  else { 
-				  response.write('\n\nNo data to log. Cancelled log operation.');
-				  response.end();
-				}
-	    });
-		  break;
-		
-		case '/ping' :
-		  response.write('\n \nFoliage Remote Operational');
-			response.end();
-		  break;
-		
-		default : 
-		  response.end();
-		  break;
-	}
+  switch(path[1]) {
+    case '/log' :
+      var now = new Date().getTime();
+      var logFile = __dirname + '/session-logs/session-log-' + now + '.json';
+      //response.write("\n\nApplication path: " + __dirname);
+      //response.write('\n\nSaving log data to: ' + logFile + ' ...');
+      var postData = '';
+
+      request.addListener("data", function (chunk) {
+        postData += chunk;
+      });
+
+      request.addListener("end", function () {
+        if(postData && postData.length > 0) {
+          fs.open(logFile, "w+", 0644, function (error, fd) {
+          	if (error) throw error;
+
+            fs.writeSync(fd, postData, 0, 'ascii');
+            response.write('\n\nLog data saved successfully');
+            response.write('\n\nLog Data: \n' + postData);
+          	response.end();
+        	});
+        }
+        else {
+          response.write('\n\nNo data to log. Cancelled log operation.');
+          response.end();
+        }
+      });
+      break;
+
+    case '/ping' :
+      response.write('\n \nFoliage Remote Operational');
+      response.end();
+      break;
+
+    default :
+      response.end();
+      break;
+  }
 }).listen(80);
